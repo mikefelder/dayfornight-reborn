9206f3a4e0d98068fa6d85f72bbf4af6aa82fbcb
Mike Felder <2172656+mikefelder@users.noreply.github.com>
2026-09-23 08:38:38 -0500

    feat: update social media links and icons across the site
    - Replaced Facebook links with GitHub links in various JSON snapshots and site configuration.
    - Updated social media icons in the footer and side navigation components to reflect the new GitHub icon.
    - Modified the social icons component to include the GitHub SVG icon.
    - Adjusted the index pages for 2015 to use the new GitHub link and icon.

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -16,8 +16,8 @@ export const SITE_DOMAIN = 'dayfornight.dev';
 export const ASSETS_BASE_URL = '';
 
 export const SOCIAL_LINKS = {
-  facebook: '#',
-  instagram: 'https://instagram.com/dayfornightfest/',
+  github: 'https://github.com/mikefelder',
+  instagram: 'https://instagram.com/slashmiek',
   twitter: 'https://twitter.com/dayfornightfest',
   spotify: 'https://open.spotify.com/playlist/5yQVK7rXvuwcna4Gx9PYVn',
 };
